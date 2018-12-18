06af88fa4ffaa5764952f1b10c7a430f96a00caa
Matias Bravo <mbravo@localhost.localdomain>
2018-12-18 05:45:56 -0300

    Added functionality to plots_1d.hist(), now it is possible to make PDF plots

--- splotch/plots_1d.py
+++ splotch/plots_1d.py
@@ -1,7 +1,7 @@
 #### Definition of all wrappers for 1D plotting
 
 #Histogram
-def hist(data,bin_num=None,dens=True,norm=1,c=None,xinvert=False,xlim=None,ylim=None,yinvert=False,xlog=False,ylog=True,
+def hist(data,bin_num=None,dens=True,norm=None,c=None,xinvert=False,xlim=None,ylim=None,yinvert=False,xlog=False,ylog=True,
 			title=None,xlabel=None,ylabel=None,plabel=None,lab_loc=0,ax=None,multi=False):
 	import numpy as np
 	import matplotlib.colors as clr
@@ -17,16 +17,10 @@ def hist(data,bin_num=None,dens=True,norm=1,c=None,xinvert=False,xlim=None,ylim=
 		bin_num=[int((len(d))**0.4) for d in data]
 	if type(bin_num) is not list:
 		bin_num=[bin_num+1]*L
-	if dens:
-		if type(norm)!=int and type(norm)!=float:
-			norm=[norm[i]*len(data[i]) for i in range(L)]
-		else:
-			norm=[norm*len(d) for d in data]
-	else:
-		if type(norm)!=int and type(norm)!=float:
-			norm=[norm[i] for i in range(L)]
-		else:
-			norm=[norm for d in data]
+	if type(dens) is not list:
+		dens=[dens]*L
+	if type(norm) is not list:
+		norm=[norm]*L
 	if c is None or type(c)==str:
 		c=[c]*L
 	if type(plabel) is not list:
@@ -35,12 +29,11 @@ def hist(data,bin_num=None,dens=True,norm=1,c=None,xinvert=False,xlim=None,ylim=
 		if xlog:
 			bins=np.logspace(np.log10(np.nanmin(data[i])),np.log10(np.nanmax(data[i])),num=bin_num[i])
 		else:
-			if np.nanmin(data[i])==np.nanmax(data[i]):
-				bins=np.linspace(np.nanmin(data[i])-0.5,np.nanmax(data[i])+0.5,num=bin_num[i])
-			else:
-				bins=np.linspace(np.nanmin(data[i]),np.nanmax(data[i]),num=bin_num[i])
-		y,x=np.histogram(data[i],bins=bins,density=dens)
-		y*=norm[i]
+			bins=np.linspace(np.nanmin(data[i]),np.nanmax(data[i]),num=bin_num[i])
+		y,x=np.histogram(data[i],bins=bins,density=dens[i])
+		if dens[i]:
+			if norm[i]:
+				y*=1.0*len(data[i])/norm[i]
 		plt.plot((x[0:-1]+x[1:])/2,y,label=plabel[i],rasterized=True)
 	if plabel[0] is not None:
 		plt.legend(loc=lab_loc)
