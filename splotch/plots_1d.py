916f0900450b6ed7723096404bfe33acb16bf6e1
Robin Cook <RobinCook9012@gmail.com>
2019-02-20 11:23:56 +0800

    Added generalised axis line
    The axline() function can now be used to call horizontal (with 'y' parameter), vertical (with 'x' parameter) or diagonal (with 'm' and 'c' parameters) lines. Currently, one can call only an array of x's individually or y's individually or m's and c's together to avoid ambiguity in deciding which line_pars are associated with which line.

--- splotch/plots_1d.py
+++ splotch/plots_1d.py
@@ -177,30 +177,110 @@ def histstep(data,bin_num=None,dens=True,xlim=None,ylim=None,xinvert=False,yinve
 	if ax is not None:
 		old_axes=axes_handler(old_axes)
 
-# Generalized lines   NEEDS TO BE REDESIGNED
-#def line(x,y,n=10,xlim=None,ylim=None,xinvert=False,yinvert=False,cinvert=False,xlog=False,ylog=False,title=None,
-#			xlabel=None,ylabel=None,plabel=None,lab_loc=0,ax=None,plot_par={},multi=False):
-#	import numpy as np
-#	import matplotlib.pyplot as plt
-#	from .base_func import axes_handler,plot_finalizer
-#	
-#	if ax is not None:
-#		old_axes=axes_handler(ax)
-#	if xlog:
-#		x=np.logspace(np.log10(x[0]),np.log10(x[1]),num=n)
-#	else:
-#		x=np.linspace(x[0],x[1],num=n)
-#	if ylog:
-#		y=np.logspace(np.log10(y[0]),np.log10(y[1]),num=n)
-#	else:
-#		y=np.linspace(y[0],y[1],num=n)
-#	plt.plot(x,y,color=c,linestyle=line_style,label=plabel,**plot_par)
-#	if plabel is not None:
-#		plt.legend(loc=lab_loc)
-#	if not multi:
-#		plot_finalizer(xlog,ylog,xlim,ylim,title,xlabel,ylabel,xinvert,yinvert)
-#	if ax is not None:
-#		old_axes=axes_handler(ax)
+
+# Generalized lines
+def axline(x=None,y=None,m=None,c=None,ax=None,line_par={}):
+	
+	"""Generalised axis lines.
+	
+	This function aims to generalise the usage of axis lines calls (axvline/axhline) together and to allow
+	lines to be specified by a slope/intercept.
+	
+	Parameters
+	----------
+	x : int or list, optional 
+		x position(s) in data coordinates for a vertical line(s).
+	y : int or list, optional
+		y position(s) in data coordinates for a horizontal line(s).
+	m : int or list, optional
+		Slope(s) of diagonal axis line(s), defaults to 1 if not specified when c is given.
+	c : int or list, optional
+		Intercept points(s) of diagonal axis line(s), defaults to 0 if not specified when m is given.
+	ax : pyplot.Axes, optional
+		Use the given axes to make the plot, defaults to the current axes.
+	line_par : dict, optional
+		Passes the given dictionary as a kwarg to the plotting function. Valid kwargs are Line2D properties.
+	Returns
+	-------
+	None
+	"""
+	    
+	import numpy as np
+	import matplotlib.pyplot as plt
+	from .base_func import axes_handler,plot_finalizer,dict_splicer
+	
+	if ax is not None:
+		old_axes=axes_handler(ax)
+	else:
+		ax=plt.gca()
+		old_axes=ax        
+	
+	if (not any([x,y,m,c])): # If nothing has been specified
+		raise TypeError("axline() missing one of optional arguments: 'x', 'y', 'm' or 'c'")
+	    
+	for i, val in enumerate([x,y,m,c]):
+		if (val is not None):
+			try: # Test whether the parameter is iterable
+				temp=(k for k in val)
+			except TypeError: # If not, convert to a list
+				if   (i == 0): x = [x]
+				elif (i == 1): y = [y]
+				elif (i == 2): m = [m]
+				elif (i == 3): c = [c]
+	
+	if (x is not None and y is not None): # Check whether both x and y were specified
+		raise ValueError("'x' and 'y' cannot be both specified")
+	
+	if (x is not None): # Check conditions if x specified
+		if (any([m,c])): # Should not specify m or c, if x given.
+			raise ValueError("'{0}' cannot be specified if x specified".format('m' if m else 'c'))
+		L = len(x)
+	    
+	if (y is not None): # Check conditions if y specified
+		if (any([m,c])): # Should not specify m or c, if y given.
+			raise ValueError("'{0}' cannot be specified if y specified".format('m' if m else 'c'))
+		L = len(y)
+	
+	if (m is not None):
+		if (c is None): # If no intercept specified
+			c = [0]*len(m) # set c to 0 for all m
+		else:
+			if (len(c) == 1):
+				c = [c[0]]*len(m)
+			elif (len(c) != len(m)):
+				if (len(m) == 1):
+					m = [m[0]]*len(c)
+				else:
+					raise ValueError("Length of c ({0}) and length of m ({1}) must be equal or otherwise 1".format(len(c),len(m)))
+		L = len(m)
+	elif (c is not None):
+		if (m is None): # If no slope specified
+			m = [1]*len(c) # set m to 1 for all c
+		L = len(c)
+	
+	# Organise the line parameters dictionary    
+	line_par=dict_splicer(line_par,L,[1]*L) 
+	    
+	if (x is not None):
+		for ii, xx in enumerate(x):
+			ax.axvline(x=xx,**line_par[ii])
+	if (y is not None):
+		for ii, yy in enumerate(y):
+			ax.axhline(y=yy,**line_par[ii])
+	if (m is not None):
+		for ii, pars in enumerate(zip(m,c)):
+			mm = pars[0]; cc = pars[1]
+			
+			xLims = ax.get_xlim()
+			yLims = ax.get_ylim()
+			
+			ax.plot([xLims[0],xLims[1]],[mm*xLims[0]+cc,mm*xLims[1]+cc],**line_par[ii])
+			
+			ax.set_xlim(xLims)
+			ax.set_ylim(yLims)
+			
+	if ax is not None:
+		old_axes=axes_handler(old_axes)
 
 #Plots
 def plot(x,y=None,xlim=None,ylim=None,xinvert=False,yinvert=False,xlog=False,ylog=False,title=None,xlabel=None,ylabel=None,
