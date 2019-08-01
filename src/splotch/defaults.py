116ce32fae2f9040725c2a9595e062caaf2f6451
Matias Bravo <mbravo@localhost.localdomain>
2019-08-01 02:44:06 -0400

    Re-added defaults.py

---
+++ src/splotch/defaults.py
@@ -0,0 +1,14 @@
+#Object containing the default values for SPLOTCH functions
+class Params:
+	#Grid
+	grid=False
+	grid_which='major'
+	grid_axis='both'
+	#Histograms
+	hist1D_output=False
+	hist1D_smooth=False
+	hist1D_yaxis_log=False
+	hist2D_caxis_log=False
+	hist2D_output=False
+	#Images
+	img_caxis_log=False
